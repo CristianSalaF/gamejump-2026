d94d848b8b54ba6ff145b123135d160ca8ab8ec1
Yeray <yeray.rodriguez.7e5@itb.cat>
2026-05-16 10:35:10 +0200

    Complete Movement Missing Shield

--- Assets/Scripts/IBlocker.cs
+++
@@ -1,9 +0,0 @@
-/// <summary>
-/// Interface segregation: exposes only the blocking state that PlayerHealth needs.
-/// This breaks the concrete dependency on BlockHandler (DIP fix).
-/// </summary>
-public interface IBlocker
-{
-    bool IsBlocking { get; }
-    float DamageReductionPercent { get; }
-}
--- Assets/Scripts/IDamageable.cs
+++
@@ -1,7 +0,0 @@
-/// <summary>
-/// Interface segregation: anything in the world that can receive damage implements this.
-/// </summary>
-public interface IDamageable
-{
-    void TakeDamage(float amount);
-}
